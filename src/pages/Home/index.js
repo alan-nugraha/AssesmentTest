14c8ca83e40df06f477a10688732e45894a4b6b6
Alan Nugraha <yaelahlan@gmail.com>
2020-10-05 22:53:53 +0700

    add pages Home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import {StyleSheet, SafeAreaView} from 'react-native';
+import {Header, Card} from '../../components';
+import axios from 'axios';
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
+  getData = () => {
+    axios
+      .get('https://api.github.com/users/alannugraha1/repos?page=1')
+      .then((response) => {
+        const data = response.data;
+        this.setState({data});
+      });
+  };
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Header />
+        {this.state.data.map((item, index) => (
+          <Card key={index} title={item.name} description={item.description} />
+        ))}
+      </SafeAreaView>
+    );
+  }
+}
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    backgroundColor: 'white',
+  },
+  textEmptyData: {
+    textAlign: 'center',
+    width: '100%',
+    marginTop: 16,
+  },
+});
